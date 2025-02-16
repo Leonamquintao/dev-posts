96bef2f831a1e43d082af1ebd367f43cd048e857
Leonam Quintão <leonam.quintao@hotelengine.com>
2025-02-16 17:15:24 -0300

    added before_action method to posts route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,11 @@
 # and you can use them in your views
 
 class PostsController < ApplicationController
+  # with this, the methods inside the array will no longer the request
+  # of having  "@post = Post.find(params[:id])" on the declaration, it uses
+  # the private before_action method declared on the bottom of the file.
+  # I just used these methods as example personally I prefer the verbose way.
+  before_action :find_blog_post, only: %i[activate deactivate destroy]
   def index
     # @posts = Post.all
     # Find only the actives one
@@ -42,7 +47,6 @@ class PostsController < ApplicationController
   end
 
   def activate
-    @post = Post.find(params[:id])
     if @post.update(active: true)
       render json: @post, status: :ok
     else
@@ -52,7 +56,6 @@ class PostsController < ApplicationController
   end
 
   def deactivate
-    @post = Post.find(params[:id])
     if @post.update(active: false)
       render json: @post, status: :ok
     else
@@ -71,7 +74,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     if @post.destroy
       render json: { message: "Post deleted" }, status: :ok
     else
@@ -88,5 +90,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body, :author)
+  end
+
+  def find_blog_post
+    @post = Post.find(params[:id])
   end
 end
